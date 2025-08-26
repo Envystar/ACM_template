ecb6cc7200ffd7ad787aa41e5a9aac9517c56acd
Chen Mu <3331400783@qq.com>
2025-08-26 19:31:42 +0800

    refactor: 重构字符串哈希

--- src/字符串/字符串哈希.cpp
+++ src/字符串/字符串哈希.cpp
@@ -1,53 +1,73 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 
-const int NUM = 2, MAXLEN = 60000;//哈希次数，字符串最大长度
-const std::vector<i64> base = {31, 37, 233};
-const std::vector<i64> mod = {2013265921, 1004535809, 2147483647};
-std::vector<std::array<i64, NUM>> fac(MAXLEN + 1);
-struct Hash {
-    Hash() {}
-    Hash(const std::string &s) : n(s.size()), hs(s.size() + 1) {//0-index
-        for(int j = 0; j < NUM; ++j) {
-            for(int i = 1; i <= n; ++i) {
-                hs[i][j] = (hs[i - 1][j] * base[j] + s[i - 1]) % mod[j];
-            }
-        }
+struct H64 {
+    H64() = default;
+    H64(i64 v) : v(v) {}
+    H64 operator+(const H64 &o) const {
+        return (v + o.v >= MOD ? v + o.v - MOD : v + o.v);
     }
-    std::array<i64, NUM> range(int l, int r) {//1-index
-        std::array<i64, NUM> res;
-        for(int i = 0; i < NUM; ++i) {
-            res[i] = (hs[r][i] - hs[l - 1][i] * fac[r - l + 1][i] % mod[i] + mod[i]) % mod[i];
-        }
-        return res;
+    H64 operator-(const H64 &o) const {
+        return (o.v > v ? v + MOD - o.v : v - o.v);
     }
-    int n;
-    std::vector<std::array<i64, NUM>> hs;
+    H64 operator*(const H64 &o) const {
+        __int128 r = (__int128)v * o.v;
+        return H64((r >> 61) + (r & MOD));
+    }
+    auto operator<=>(const H64& o) const = default;
+    i64 v = 0;
+    constexpr static i64 MOD = (1LL << 61) - 1;
 };
 
-void HashInit() {
-    for(int j = 0; j < NUM; ++j) {
-        fac[0][j] = 1;
-        for(int i = 1; i <= MAXLEN; ++i) {
-            fac[i][j] = fac[i - 1][j] * base[j] % mod[j];
+struct Hash {
+    Hash() = default;
+    Hash(const Hash &hs) : h(hs.h), n(1) {}
+    Hash(const int &val) : h(H64(val)), n(1) {}
+    Hash(const std::string &s) : n(s.size()) {
+        for(int i = 0; i < (int)s.size(); ++i) {
+            h = h + H64(s[i]) * fac(s.size() - i - 1);;
         }
     }
-}
+    Hash(const H64 &h, const int &n) : h(h), n(n) {}
+    auto operator<=>(const Hash& o) const = default;
+    Hash operator+(const Hash &o) {
+        return {h * fac(o.n) + o.h, n + o.n};
+    }
+    Hash operator-(const Hash &o) {
+        return {h - o.h * fac(n - o.n), n - o.n};
+    }
+    Hash operator+=(const Hash &o) {
+        return *this = (*this + o);
+    }
+    Hash operator-=(const Hash &o) {
+        return *this = (*this - o);
+    }
+    static H64 fac(int x) {
+        if(x >= f.size()) {
+            int l = f.size();
+            f.resize(x + 1);
+            for(int i = l; i < f.size(); ++i) {
+                f[i] = f[i - 1] * BASE;
+            }
+        }
+        return f[x];
+    }
+    int n{};
+    H64 h{};
+    constexpr static i64 BASE = 114514;
+    inline static std::vector<H64> f{H64(1)};
+};
 
-//字符串hash
-//https://www.luogu.com.cn/problem/P3370
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    HashInit();//预处理
     int n;
     std::cin >> n;
-    std::set<std::array<i64, NUM>> st;
+    std::set<Hash> st;
     for(int i = 0; i < n; ++i) {
         std::string s;
         std::cin >> s;
-        Hash hs(s);
-        st.insert(hs.range(1, s.size()));
+        st.insert(Hash(s));
     }
     std::cout << st.size() << '\n';
     return 0;
