0b225eb41de2a5dee09662bcf9ceb944243910a6
Chen Mu <3331400783@qq.com>
2025-09-05 15:55:26 +0800

    refactor: 重写矩阵

--- src/数学/线性代数/矩阵.cpp
+++ src/数学/线性代数/矩阵.cpp
@@ -3,60 +3,64 @@ using i64 = long long;
 
 template<typename T>
 struct Matrix {
-    Matrix() : n(0), m(0) {};
-    Matrix(int _n, int _m) : n(_n), m(_m), mt(n, std::vector<T>(m)){}
-    Matrix(const std::vector<std::vector<T>> &v) : Matrix(v.size(), v[0].size()) {
-        for(int i = 0; i < n; ++i) {
-            assert(v[i].size() == m);
-            for(int j = 0; j < m; ++j) {
-                mt[i][j] = v[i][j];
+    Matrix() : Matrix(0, 0) {};
+    Matrix(int _n, int _m, T val = T{}) : n(_n), m(_m), mt(n * m, val){}
+    Matrix(const std::initializer_list<std::initializer_list<T>> &v) : Matrix(v.size(), v.begin()->size()) {
+        int i = 0;
+        for(auto &row : v) {
+            assert(row.size() == m);
+            for(auto &x : row) {
+                mt[i++] = x;
             }
         }
     }
-    Matrix<T> operator*(const Matrix<T> &o) {
-        assert(m == o.n);
-        Matrix<T> res(n, o.m);
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < o.m; ++j) {
-                for(int k = 0; k < m; ++k) {
-                    res.mt[i][j] = res.mt[i][j] + mt[i][k] * o.mt[k][j];
+    std::span<T> operator[](int idx) {
+        return std::span<T>(mt.data() + idx * m, m);
+    }
+    std::span<const T> operator[](int idx) const {
+        return std::span<const T>(mt.data() + idx * m, m);
+    }
+    friend Matrix<T> operator*(const Matrix<T>& A, const Matrix<T>& B) {
+        assert(A.m == B.n);
+        Matrix<T> MT(A.n, B.m);
+        for(int i = 0; i < MT.n; ++i) {
+            for(int j = 0; j < MT.m; ++j) {
+                for(int k = 0; k < A.m; ++k) {
+                    MT[i][j] = MT[i][j] + A[i][k] * B[k][j];
                 }
             }
         }
-        return res;
+        return MT;
     }
-    Matrix<T> operator*=(const Matrix<T> &o) {
-        return *this = *this * o;
-    }
-    Matrix<T> operator+(const Matrix<T> &o) {
-        assert(n == o.n && m == o.m);
-        Matrix<T> res(n, m);
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                res.mt[i][j] = mt[i][j] + o.mt[i][j];
+    friend Matrix<T> operator+(const Matrix<T>& A, const Matrix<T>& B) {
+        assert(A.n == B.n && A.m == B.m);
+        Matrix<T> MT(A.n, B.m);
+        for(int i = 0; i < MT.n; ++i) {
+            for(int j = 0; j < MT.m; ++j) {
+                MT[i][j] = A[i][j] + B[i][j];
             }
         }
-        return res;
+        return MT;
     }
-    Matrix<T> operator-(const Matrix<T> &o) {
-        assert(n == o.n && m == o.m);
-        Matrix<T> res(n, m);
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                res.mt[i][j] = mt[i][j] - o.mt[i][j];
+    friend Matrix<T> operator-(const Matrix<T>& A, const Matrix<T>& B) {
+        assert(A.n == B.n && A.m == B.m);
+        Matrix<T> MT(A.n, B.m);
+        for(int i = 0; i < MT.n; ++i) {
+            for(int j = 0; j < MT.m; ++j) {
+                MT[i][j] = A[i][j] - B[i][j];
             }
         }
-        return res;
+        return MT;
     }
     static Matrix<T> eye(int n) {
-        Matrix<T> res(n, n);
+        Matrix<T> MT(n, n);
         for(int i = 0; i < n; ++i) {
-            res.mt[i][i] = 1;
+            MT[i][i] = 1;
         }
-        return res;
+        return MT;
     }
     static Matrix<T> qpow(Matrix<T> a, i64 b) {
-        Matrix<T> res(Matrix::eye(a.n));
+        Matrix<T> res(Matrix<T>::eye(a.n));
         while(b != 0) {
             if(b & 1) {
                 res = res * a;
@@ -69,20 +73,18 @@ struct Matrix {
     friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& o) {
         for(int i = 0; i < o.n; ++i) {
             for(int j = 0; j < o.m; ++j) {
-                os << o.mt[i][j] << " \n"[j + 1 == o.m];
+                os << o[i][j] << " \n"[j + 1 == o.m];
             }
         }
         return os;
     }
     int n, m;
-    std::vector<std::vector<T>> mt;
+    std::vector<T> mt;
 };
 
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    Matrix<int> res({{1, 2}, {2, 3}});
-    Matrix<int> b(res);
-    std::cout << Matrix<int>::qpow(res, 3);
+
     return 0;
 }
