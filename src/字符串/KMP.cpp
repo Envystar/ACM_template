2eb260659dfd4e497d7b363ba56bfcc91fd83726
Chen Mu <3331400783@qq.com>
2025-09-05 15:56:13 +0800

    refactor: 封装KMP函数

--- src/字符串/KMP.cpp
+++ src/字符串/KMP.cpp
@@ -1,37 +1,33 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 
+std::vector<int> kmp(const std::string &s) {
+    std::vector<int> v(s.size());
+    for(int i = 1, j = 0; i < s.size(); ++i) {
+        while(j > 0 && s[i] != s[j]) {
+            j = v[j - 1];
+        }
+        if(s[i] == s[j]) {
+            ++j;
+        }
+        v[i] = j;
+    }
+    return v;
+}
+
 int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::string s, p;
     std::cin >> s >> p;
-    int n = s.size(), m = p.size();
-    s = '#' + s, p = '#' + p;
-    std::vector<int> kmp(m + 1);
-    for(int i = 2, j = 0; i <= m; ++i) { //求kmp数组
-        while(j > 0 && p[i] != p[j + 1]) {
-            j = kmp[j];
+    auto v = kmp(p + "#" + s);
+    for(int i = p.size() + 1; i < v.size(); ++i) {
+        if(v[i] == p.size()) {
+            std::cout << i - 2 * p.size() + 1 << '\n';
         }
-        if(p[j + 1] == p[i]) {
-            j++;
-        }
-        kmp[i] = j;
     }
-    for(int i = 1, j = 0; i <= n; ++i) {
-        while(j > 0 && s[i] != p[j + 1]) {
-            j = kmp[j];
-        }
-        if(s[i] == p[j + 1]) {
-            j++;
-        }
-        if(j == m) {
-            std::cout << i - j + 1 << '\n';
-            j = kmp[j];
-        } 
-    }
-    for(int i = 1; i <= m; ++i) {
-        std::cout << kmp[i] << " \n"[i == m];
+    for(int i = 0; i < p.size(); ++i) {
+        std::cout << v[i] << " \n"[i + 1 == p.size()];
     }
     return 0;
 }
